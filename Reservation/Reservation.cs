aad9dbcc4c391f910192676f47246161c48efe4f
unittest1107 <52784736+unittest1107@users.noreply.github.com>
2019-07-11 14:21:10 +0300

    Add files via upload

---
+++ Reservation/Reservation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanbeCancelledBy(User user)
+        {
+            if (user.IsAdmin)
+                return true;
+
+            if (user == MadeBy)
+                return true;
+
+            return false;
+        }
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ Reservation/ReservationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanbeCancelledBy_AdminCancel_ReturnTrue()
+        {
+            // AAA
+            // Arrange
+            Reservation r = new Reservation();
+
+            // Act
+            bool result = r.CanbeCancelledBy(new User { IsAdmin = true });
+
+            // Assert
+            Assert.IsTrue(result);
+
+            //Assert.That.
+        }
+
+
+    }
+}
