51b23d33f9181dac480b22a284d77569b988db25
unittest1107 <52784736+unittest1107@users.noreply.github.com>
2019-07-11 15:18:01 +0300

    Add files via upload

---
+++ Reservation/Step3/Reservation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanbeCancelledBy(User user)
+        {
+            if (user == null)
+                throw new NullUserException("Null user given for CanbeCancelledBy");
+
+            if (user.IsAdmin)
+                return true;
+
+            if (user == MadeBy)
+                return true;
+
+            return false;
+        }
+
+        public float CalcAverageTables(int tables, int hours)
+        {
+            if (tables < 0 || hours < 0)
+                throw new ArgumentException($"Wrong input to method CalcAverageTables: "
+                    + $"hours : {hours} tables : {tables}");
+
+            return tables / hours;
+        }
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+
+    
+}
---
+++ Reservation/Step3/ReservationTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanbeCancelledBy_AdminCancel_ReturnTrue()
+        {
+            // AAA
+            // Arrange
+            Reservation r = new Reservation();
+
+            // Act
+            bool result = r.CanbeCancelledBy(new User { IsAdmin = true });
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanbeCancelledBy_SameNonAdminCancel_ReturnTrue()
+        {
+            // AAA
+            // Arrange
+            Reservation r = new Reservation();
+            User u = new User();
+            r.MadeBy = u;
+
+            // Act
+            bool result = r.CanbeCancelledBy(u);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanbeCancelledBy_DiffNonAdminCancel_ReturnFalse()
+        {
+            // AAA
+            // Arrange
+            Reservation r = new Reservation();
+            User u = new User();
+            u.IsAdmin = false;
+            r.MadeBy = new User();
+
+            // Act
+            bool result = r.CanbeCancelledBy(u);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(NullUserException))]
+        public void CanbeCancelledBy_NullUserInput_ThrowException()
+        {
+            // AAA
+            // Arrange
+            Reservation r = new Reservation();
+
+            // Act
+            bool result = r.CanbeCancelledBy(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CalcAverageTables_InvalidInput_ThrowException()
+        {
+            // Arrange
+            Reservation r = new Reservation();
+
+            // Act
+            float f = r.CalcAverageTables(-5, -5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void CalcAverageTables_ZeroHours_ThrowException()
+        {
+            // Arrange
+            Reservation r = new Reservation();
+
+            // Act
+            float f = r.CalcAverageTables(5, 0);
+        }
+
+        [TestMethod]
+        public void CalcAverageTables_ValidCalculation_ValidResult()
+        {
+            // Arrange
+            Reservation r = new Reservation();
+
+            // Act
+            Assert.AreEqual( r.CalcAverageTables(10, 5), 2 );
+            Assert.AreEqual( r.CalcAverageTables(15, 5), 0 );
+            Assert.AreEqual( r.CalcAverageTables(20, 4), 5 );
+        }
+    }
+}
